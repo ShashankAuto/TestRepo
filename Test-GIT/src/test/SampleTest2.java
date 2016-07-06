709652e766004d6032927e73628ba84449fdd021
sony <sony@192.168.0.102>
2016-07-06 21:14:53 +0530

    Added File 2

---
+++ Test-GIT/src/test/SampleTest2.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class SampleTest2 {
+
+	@Test
+	public void sampleExample(){
+		System.out.println("In sample Git and Jenkins");
+	}
+}
