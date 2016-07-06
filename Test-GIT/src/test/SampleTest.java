7da41cdaf3b6e4d6c1efe34c14d5b3702ae6003e
sony <sony@192.168.0.102>
2016-07-06 21:10:12 +0530

    Adding file to git

---
+++ Test-GIT/src/test/SampleTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void sampleExample(){
+		System.out.println("In sample Git and Jenkins");
+	}
+}
